95e22ecdcdd473c01d99086fc784c616394255fa
Abubakar-Shehu <shehu.lanre@icloud.com>
2025-07-03 17:28:57 -0600

    Added the file reading and writing and an example txt file

--- fetcher.js
+++ fetcher.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
 const cmdInput = process.argv.slice(2);
+
+const needle = require('needle');
+needle.get(cmdInput[0], (error, response, body) => {
+  if (error) {
+    throw error
+  }
+  if(response.statusCode > 200) {
+    console.log(response)
+  }
+  const fileInBytes = body.length;
+
+  fs.writeFile(cmdInput[1], body, () => {
+    console.log(`Downloaded and saved ${fileInBytes} bytes to ${cmdInput[1]}`)
+  })
+});
+
